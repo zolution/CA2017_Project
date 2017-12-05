fb8f814a5a4f89f10495d48613ea1baf1d77d98a
ppetter1025 <peter58972@gmail.com>
2017-12-05 20:18:40 +0800

    data memory copied from StackOverflow

---
+++ pj1/hw4_code/Data_Memory.v
@@ -0,0 +1,18 @@
+module Data_Memory(clk, addr_i, w_data_i, MemRead_i, MemWrite_i, r_data_o)
+
+input	wire	[31:0]	addr_i, w_data_i;
+input	wire			MemRead_i, MemWrite_i, clk;
+output	wire	[31:0]	r_data_o;
+
+reg		[31:0]	Mem[7:0];
+
+always @(posedge clk) begin
+	if (MemRead_i == 1'b1)	begin
+		r_data_o  <= Mem[addr_i];
+	end
+
+	if (MemWrite_i == 1'b1) begin
+		Mem[addr_i] <= w_dara_i;
+end
+
+endmodule
