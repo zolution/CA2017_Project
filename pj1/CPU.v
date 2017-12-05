76959ed2d3d7717cdc881c5877dc164d95d7757d
ppetter1025 <peter58972@gmail.com>
2017-12-05 22:30:12 +0800

    add CPU.v

--- pj1/CPU.v
+++ pj1/CPU.v
@@ -6,18 +6,24 @@ module CPU
 );
 
 // Ports
-input               clk_i;
-input               rst_i;
-input               start_i;
+input	wire	clk_i;
+input			rst_i;
+input			start_i;
 
-wire [31:0]	inst_addr, inst;
+wire	[31:0]	inst_addr, inst, ALUres, RTdata;
 
 Control Control(
     .Op_i       (inst[31:26]),
     .RegDst_o   (),
     .ALUOp_o    (),
     .ALUSrc_o   (),
-    .RegWrite_o ()
+    .RegWrite_o (),
+	.MemtoReg_o	(),
+	.MemWrite_o	(),
+	.Branch_o	(),
+	.Jump_o		(),
+	.ExtOp_o	(),
+	.MemRead_o	()
 );
 
 Adder Add_PC(
@@ -44,7 +50,7 @@ Registers Registers(
     .RSaddr_i   (inst[25:21]),
     .RTaddr_i   (inst[20:16]),
     .RDaddr_i   (MUX_RegDst.data_o), 
-    .RDdata_i   (ALU.data_o),
+    .RDdata_i   (MUX_MemtoReg.data_o),
     .RegWrite_i (Control.RegWrite_o), 
     .RSdata_o   (), 
     .RTdata_o   () 
@@ -58,7 +64,7 @@ MUX5 MUX_RegDst(
 );
 
 MUX32 MUX_ALUSrc(
-    .data1_i    (Registers.RTdata_o),
+    .data1_i    (RTdata),
     .data2_i    (Sign_Extend.data_o),
     .select_i   (Control.ALUSrc_o),
     .data_o     ()
@@ -73,7 +79,7 @@ ALU ALU(
 	.data1_i    (Registers.RSdata_o),
     .data2_i    (MUX_ALUSrc.data_o),
     .ALUCtrl_i  (ALU_Control.ALUCtrl_o),
-    .data_o     (),
+    .data_o     (ALUres),
     .Zero_o     ()
 );
 
@@ -81,6 +87,22 @@ ALU_Control ALU_Control(
     .funct_i    (inst[5:0]),
     .ALUOp_i    (Control.ALUOp_o),
     .ALUCtrl_o  ()
+);
+
+Data_Memory Data_Memory(
+	.clk		(clk),
+	.addr_i		(ALUres),
+	.w_data_i	(RTdata),
+	.MemRead_i	(Control.MemRead_o),
+	.MemWrite_i	(Control.MemWrite_o),
+	.r_data_o	()
+);
+
+MUX32 MUX_MemtoReg(
+	.data1_i	(DAtaMemory.r_data_o),
+	.data2_i	(ALUres),
+	.select_i	(Control.MemtoReg),
+	.data_o		()
 );
 
 endmodule
