d95424d256e602d16ea56991989e64b459ba0313
zolution <zolution@outlook.com>
2017-12-05 22:26:11 +0800

    Add MEMWB Control Port

--- pj1/MEMWB.v
+++ pj1/MEMWB.v
@@ -2,7 +2,9 @@ module MEMWB
 (
 	clk_i,
 	mux0_i, mux1_i,
-	mux0_o, mux1_o
+	mux0_o, mux1_o,
+	RegWrite_i, MemtoReg_i,
+	RegWrite_o, MemtoReg_o
 );
 
 input			clk_i;
@@ -10,9 +12,15 @@ input	[31:0]	mux0_i, mux1_i;
 output	[31:0]	mux0_o, mux1_o;
 reg		[31:0]	mux0_o, mux1_o;
 
+input			RegWrite_i, MemtoReg_i;
+output			RegWrite_o, MemtoReg_o;
+reg   			RegWrite_o, MemtoReg_o;
+
 always@(posedge clk_i) begin
 	mux0_o <= mux0_i;
 	mux1_o <= mux1_i;
+	RegWrite_o <= RegWrite_i;
+	MemtoReg_o <= MemtoReg_i;
 end
 
 endmodule
