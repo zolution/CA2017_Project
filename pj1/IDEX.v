ba64e6949c2b50675024d01d2edf6ceb8b52762c
zolution <zolution@outlook.com>
2017-12-05 21:37:54 +0800

    Add IDEX Ctrl Signal Port

--- pj1/IDEX.v
+++ pj1/IDEX.v
@@ -2,7 +2,9 @@ module IDEX
 (
 	clk_i,
 	pc_i, data1_i, data2_i, extend_i,
-	pc_o, data1_o, data2_o, extend_o
+	pc_o, data1_o, data2_o, extend_o,
+	RegDst_i, ALUSrc_i, MemtoReg_i, RegWrite_i, MemWrite_i, Branch_i, Jump_i, ExtOp_i, ALUOp_i,
+	RegDst_o, ALUSrc_o, MemtoReg_o, RegWrite_o, MemWrite_o, Branch_o, Jump_o, ExtOp_o, ALUOp_o
 );
 
 input			clk_i;
@@ -15,6 +17,15 @@ always@(posedge clk_i) begin
 	data1_o <= data1_i;
 	data2_o <= data2_i;
 	extend_o <= extend_i;
+	RegDst_o <= RegDst_i;
+	ALUSrc_o <= ALUSrc_i;
+	MemtoReg_o <= MemtoReg_i;
+	RegWrite_o <= RegWrite_i;
+	MemWrite_o <= MemWrite_i;
+	Branch_o <= Branch_i;
+	Jump_o <= Jump_i;
+	ExtOp_o <= ExtOp_i;
+	ALUOp_o <= ALUOp_i;
 end
 
 endmodule
