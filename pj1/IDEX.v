74e7dbaaa4ab84c4078dc74e97cedd8d88c695d3
zolution <zolution@outlook.com>
2017-12-05 22:08:33 +0800

    Fix IDEX Syntax and Port Error

--- pj1/IDEX.v
+++ pj1/IDEX.v
@@ -3,14 +3,23 @@ module IDEX
 	clk_i,
 	pc_i, data1_i, data2_i, extend_i,
 	pc_o, data1_o, data2_o, extend_o,
-	RegDst_i, ALUSrc_i, MemtoReg_i, RegWrite_i, MemWrite_i, Branch_i, Jump_i, ExtOp_i, ALUOp_i,
-	RegDst_o, ALUSrc_o, MemtoReg_o, RegWrite_o, MemWrite_o, Branch_o, Jump_o, ExtOp_o, ALUOp_o
+	RegDst_i, ALUSrc_i, MemtoReg_i, RegWrite_i, MemWrite_i, Branch_i, Jump_i, ExtOp_i, ALUOp_i, MemRead_i,
+	RegDst_o, ALUSrc_o, MemtoReg_o, RegWrite_o, MemWrite_o, Branch_o, Jump_o, ExtOp_o, ALUOp_o, MemRead_o
 );
 
 input			clk_i;
 input	[31:0]	pc_i, data1_i, data2_i, extend_i;
 output	[31:0]	pc_o, data1_o, data2_o, extend_o;
 reg		[31:0]	pc_o, data1_o, data2_o, extend_o;
+
+input			RegDst_i, ALUSrc_i, MemtoReg_i, RegWrite_i, MemWrite_i, Branch_i, Jump_i, ExtOp_i, MemRead_i;
+output			RegDst_o, ALUSrc_o, MemtoReg_o, RegWrite_o, MemWrite_o, Branch_o, Jump_o, ExtOp_o, MemRead_o;
+
+input	[1:0]	ALUOp_i;
+output	[1:0]	ALUOp_o;
+
+reg 			RegDst_o, ALUSrc_o, MemtoReg_o, RegWrite_o, MemWrite_o, Branch_o, Jump_o, ExtOp_o, MemRead_o;
+reg 	[1:0]	ALUOp_o;
 
 always@(posedge clk_i) begin
 	pc_o <= pc_i;
@@ -26,6 +35,7 @@ always@(posedge clk_i) begin
 	Jump_o <= Jump_i;
 	ExtOp_o <= ExtOp_i;
 	ALUOp_o <= ALUOp_i;
+	MemRead_o <= MemRead_i;
 end
 
 endmodule
