601e16248c5bf1670ede85be0e971efccceefb2a
zolution <zolution@outlook.com>
2017-12-06 13:59:54 +0800

    Hotfix CPU.v and EXEWB.v due to removal of branch/jump

--- pj1/EXMEM.v
+++ pj1/EXMEM.v
@@ -3,8 +3,8 @@ module EXMEM
 	clk_i,
 	pc_i, ALUres_i, wrdata_i,
 	pc_o, ALUres_o, wrdata_o,
-	Branch_i, MemRead_i, MemWrite_i, RegWrite_i, MemtoReg_i,
-	Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o,
+	MemRead_i, MemWrite_i, RegWrite_i, MemtoReg_i,
+	MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o,
 	WriteBackPath_i, WriteBackPath_o
 );
 
@@ -14,9 +14,9 @@ output	[31:0]	pc_o, ALUres_o, wrdata_o;
 reg		[31:0]	pc_o, ALUres_o, wrdata_o;
 
 // Control Signal
-input			Branch_i, MemRead_i, MemWrite_i, RegWrite_i, MemtoReg_i;
-output			Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o;
-reg 			Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o;
+input			MemRead_i, MemWrite_i, RegWrite_i, MemtoReg_i;
+output			MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o;
+reg 			MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o;
 
 // Write path
 input 	[4:0]	WriteBackPath_i;
@@ -28,7 +28,6 @@ always@(posedge clk_i) begin
 	ALUres_o <= ALUres_i;
 	wrdata_o <= wrdata_i;
 	// Control signal
-	Branch_o <= Branch_i;
 	MemRead_o <= MemRead_i;
 	MemWrite_o <= MemWrite_i;
 	RegWrite_o <= RegWrite_i;
