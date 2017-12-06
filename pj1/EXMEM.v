59b72d142edc4272e62086365b1a86a4de3fe9ce
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-12-06 11:50:15 +0800

    Add writeback path ports on the 3 interface

--- pj1/EXMEM.v
+++ pj1/EXMEM.v
@@ -4,7 +4,8 @@ module EXMEM
 	pc_i, ALUres_i, wrdata_i,
 	pc_o, ALUres_o, wrdata_o,
 	Branch_i, MemRead_i, MemWrite_i, RegWrite_i, MemtoReg_i,
-	Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o
+	Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o,
+	WriteBackPath_i, WriteBackPath_o
 );
 
 input			clk_i;
@@ -12,19 +13,28 @@ input	[31:0]	pc_i, ALUres_i, wrdata_i;
 output	[31:0]	pc_o, ALUres_o, wrdata_o;
 reg		[31:0]	pc_o, ALUres_o, wrdata_o;
 
+// Control Signal
 input			Branch_i, MemRead_i, MemWrite_i, RegWrite_i, MemtoReg_i;
 output			Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o;
 reg 			Branch_o, MemRead_o, MemWrite_o, RegWrite_o, MemtoReg_o;
+
+// Write path
+input 	[4:0]	WriteBackPath_i;
+output 	[4:0]	WriteBackPath_o;
+reg 	[4:0]	WriteBackPath_o;
 
 always@(posedge clk_i) begin
 	pc_o <= pc_i;
 	ALUres_o <= ALUres_i;
 	wrdata_o <= wrdata_i;
+	// Control signal
 	Branch_o <= Branch_i;
 	MemRead_o <= MemRead_i;
 	MemWrite_o <= MemWrite_i;
 	RegWrite_o <= RegWrite_i;
 	MemtoReg_o <= MemtoReg_i;
+	// Writeback path
+	WriteBackPath_o <= WriteBackPath_i;
 end
 
 endmodule
--- pj1/MEMWB.v
+++ pj1/MEMWB.v
@@ -4,7 +4,8 @@ module MEMWB
 	mux0_i, mux1_i,
 	mux0_o, mux1_o,
 	RegWrite_i, MemtoReg_i,
-	RegWrite_o, MemtoReg_o
+	RegWrite_o, MemtoReg_o,
+	WriteBackPath_i, WriteBackPath_o
 );
 
 input			clk_i;
@@ -12,15 +13,24 @@ input	[31:0]	mux0_i, mux1_i;
 output	[31:0]	mux0_o, mux1_o;
 reg		[31:0]	mux0_o, mux1_o;
 
+// Control signal
 input			RegWrite_i, MemtoReg_i;
 output			RegWrite_o, MemtoReg_o;
 reg   			RegWrite_o, MemtoReg_o;
 
+// Write path
+input 	[4:0]	WriteBackPath_i;
+output 	[4:0]	WriteBackPath_o;
+reg 	[4:0]	WriteBackPath_o;
+
 always@(posedge clk_i) begin
 	mux0_o <= mux0_i;
 	mux1_o <= mux1_i;
+	// Control signal
 	RegWrite_o <= RegWrite_i;
 	MemtoReg_o <= MemtoReg_i;
+	// Writeback path
+	WriteBackPath_o <= WriteBackPath_i;
 end
 
 endmodule
