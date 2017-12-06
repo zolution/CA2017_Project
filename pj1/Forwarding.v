498bf46eaa20c85ff4222f83cdc54f3699ad65b6
ppetter1025 <peter58972@gmail.com>
2017-12-06 11:21:30 +0800

    fix forwarding unit QQ

--- pj1/Forwarding.v
+++ pj1/Forwarding.v
@@ -5,9 +5,9 @@ input			EM_RegWrite_i, MW_RegWrite_i;
 output	[1:0]	ForwardA_o, ForwardB_o;
 
 assign FowardA_o = (EM_RegWrite_i==1'b1 && EM_RegRD_i!=5'b0 && EM_RegRD_i==IE_RegRS_i) ? 10 :
-				   (MW_RegWrite_i==1'b1 && MW_RegRD_i!=5'b0 && MW_RegRD_i==IE_RegRS_i) ? 01 : 00;
+				   (MW_RegWrite_i==1'b1 && MW_RegRD_i!=5'b0 && EM_RegRd_i!=IE_RegRS_i && MW_RegRD_i==IE_RegRS_i) ? 01 : 00;
 
 assign FowardB_o = (EM_RegWrite_i==1'b1 && EM_RegRD_i!=5'b0 && EM_RegRD_i==IE_RegRT_i) ? 10 :
-				   (MW_RegWrite_i==1'b1 && MW_RegRD_i!=5'b0 && MW_RegRD_i==IE_RegRT_i) ? 01 : 00;
+				   (MW_RegWrite_i==1'b1 && MW_RegRD_i!=5'b0 && EM_RegRD_i!=IE_RegRT_i && MW_RegRD_i==IE_RegRT_i) ? 01 : 00;
 
 endmodule
