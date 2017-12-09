3ee86d7b49540b63b81e906f2a4b53f139ef019e
ppetter1025 <peter58972@gmail.com>
2017-12-09 16:24:07 +0800

    initial value of Data_Memory & 2'b in Forwarding

--- pj1/Data_Memory.v
+++ pj1/Data_Memory.v
@@ -2,7 +2,7 @@ module Data_Memory(clk, addr_i, w_data_i, MemRead_i, MemWrite_i, r_data_o);
 
 input	wire	[31:0]	addr_i, w_data_i;
 input	wire			MemRead_i, MemWrite_i, clk;
-output	reg		[31:0]	r_data_o;
+output	reg		[31:0]	r_data_o = 32'b0;
 
 reg		[31:0]	memory[31:0];
 
